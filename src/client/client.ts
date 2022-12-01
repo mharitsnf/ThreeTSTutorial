fd50e97f5355ab7dbf5d5a37f70e2ee9f10f27b9
Harits <mharitsnf@gmail.com>
2022-12-01 11:00:11 +0100

    Finished lesson 17

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,8 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
 
-
-// Camera setup ==================================================
+// ==================================================
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -13,14 +12,14 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.z = 3
 
-// Renderer setup ==================================================
+// ==================================================
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera, renderer.domElement)
 
-// Object3D setup ==================================================
+// ==================================================
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
@@ -30,12 +29,14 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
 const planeGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const material = new THREE.MeshBasicMaterial({
-    // color: 0x00ff00,
-    // wireframe: true,
-})
+const material = new THREE.MeshBasicMaterial() //{ color: 0x00ff00, wireframe: true })
 
-// const material= new THREE.MeshNormalMaterial()
+const texture = new THREE.TextureLoader().load("img/grid.png")
+material.map = texture
+const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
+envTexture.mapping = THREE.CubeReflectionMapping
+// envTexture.mapping = THREE.CubeRefractionMapping
+material.envMap = envTexture
 
 const cube = new THREE.Mesh(boxGeometry, material)
 cube.position.x = 5
@@ -57,7 +58,7 @@ const torusKnot = new THREE.Mesh(torusKnotGeometry, material)
 torusKnot.position.x = -5
 scene.add(torusKnot)
 
-// Listener setup ==================================================
+// ==================================================
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -66,16 +67,21 @@ function onWindowResize() {
     render()
 }
 
-// Debug setup ==================================================
+// ==================================================
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
 const options = {
     side: {
-        "FrontSide": THREE.FrontSide,
-        "BackSide": THREE.BackSide,
-        "DoubleSide": THREE.DoubleSide,
-    }
+        FrontSide: THREE.FrontSide,
+        BackSide: THREE.BackSide,
+        DoubleSide: THREE.DoubleSide,
+    },
+    combine: {
+        MultiplyOperation: THREE.MultiplyOperation,
+        MixOperation: THREE.MixOperation,
+        AddOperation: THREE.AddOperation,
+    },
 }
 
 const gui = new GUI()
@@ -84,17 +90,35 @@ materialFolder.add(material, 'transparent').onChange(() => material.needsUpdate 
 materialFolder.add(material, 'opacity', 0, 1, 0.01)
 materialFolder.add(material, 'depthTest')
 materialFolder.add(material, 'depthWrite')
-materialFolder.add(material, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
+materialFolder
+    .add(material, 'alphaTest', 0, 1, 0.01)
+    .onChange(() => updateMaterial())
 materialFolder.add(material, 'visible')
-materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
+materialFolder
+    .add(material, 'side', options.side)
+    .onChange(() => updateMaterial())
 materialFolder.open()
+
+const data = {
+    color: material.color.getHex(),
+}
+
+const meshBasicMaterialFolder = gui.addFolder('THREE.MeshBasicMaterial')
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) })
+meshBasicMaterialFolder.add(material, 'wireframe')
+// meshBasicMaterialFolder.add(material, 'wireframeLinewidth', 0, 10)
+meshBasicMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial())
+meshBasicMaterialFolder.add(material, 'reflectivity', 0, 1)
+meshBasicMaterialFolder.add(material, 'refractionRatio', 0, 1)
+meshBasicMaterialFolder.open()
 
 function updateMaterial() {
     material.side = Number(material.side)
+    material.combine = Number(material.combine)
     material.needsUpdate = true
 }
 
-// Running the program ==================================================
+// ==================================================
 function animate() {
     requestAnimationFrame(animate)
 
